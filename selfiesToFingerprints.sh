2e4a435908d3e14bb2952ce43d1aa56b486c8a2c
zwefers <zoewefers@comcast.net>
2022-05-11 15:38:11 -0400

    updated launcher

--- selfiesToFingerprints.sh
+++ selfiesToFingerprints.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 #SBATCH --job-name=sefliesToFingerprints
-#SBATCH --output=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/cbas/logs/selfiesToFingerprints_%A.out
-#SBATCH --error=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/cbas/logs/selfiesToFingerprints_%A.err
+#SBATCH --output=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/logs/selfiesToFingerprints_%A.out
+#SBATCH --error=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/logs/selfiesToFingerprints_%A.err
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=4000M
 python selfiesToFingerprint.py --learning_rate $1 --num_epochs $2

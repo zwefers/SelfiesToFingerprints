0bf72b6433979baaf80d564997b7274d7945decb
zwefers <zoewefers@comcast.net>
2022-05-11 15:30:15 -0400

    updated launcher

--- selfiesToFingerprints.sh
+++ selfiesToFingerprints.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #SBATCH --account=def-jeromew
 #SBATCH --time=02:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
 #SBATCH --job-name=sefliesToFingerprints
 #SBATCH --output=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/cbas/logs/selfiesToFingerprints_%A.out
 #SBATCH --error=/home/zwefers/projects/def-jeromew/zwefers/SelfiesToFingerprints/cbas/logs/selfiesToFingerprints_%A.err
